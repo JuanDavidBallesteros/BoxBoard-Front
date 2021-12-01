bd0a0b85b42a0b0e7dc68bbeb4db29f77f623103
JuanDavidBallesteros <juan.ballesteros@u.icesi.edu.co>
2021-12-01 16:11:33 -0500

    fetch data (post)

--- js/index.js
+++ js/index.js
@@ -15,7 +15,7 @@ let cards = [
         date: "may 8",
         status: "doing"
 
-    }, 
+    },
     {
         id: "3242",
         title: "Test2",
@@ -37,47 +37,69 @@ let cards = [
 const load = async () => {
 
     let response = await fetch("https://box-board.herokuapp.com/api/cards");
-    if(response.ok){
+    if (response.ok) {
         let data = await response.json();
 
-    toDoCol.innerHTML = "";
-    doingCol.innerHTML = "";
-    doneCol.innerHTML = "";
+        toDoCol.innerHTML = "";
+        doingCol.innerHTML = "";
+        doneCol.innerHTML = "";
 
-    data.forEach(card => {
-        let cardView = new CardView(card);
-        if(card.status == "todo"){
-            cardView.render(toDoCol);
-        } else if (card.status == "doing"){
-            cardView.render(doingCol);
-        } else if (card.status == "done"){
-            cardView.render(doneCol);
-        }
-        
-    });
+        data.forEach(card => {
+            let cardView = new CardView(card);
+            if (card.status == "todo") {
+                cardView.render(toDoCol);
+            } else if (card.status == "doing") {
+                cardView.render(doingCol);
+            } else if (card.status == "done") {
+                cardView.render(doneCol);
+            }
+
+        });
+    } else {
+        //Alert
     }
-    
+
 }
 
 load()
 
-addCard = () => {
+addCard = async () => {
+
+    let dateObj = new Date(Date.now());
+    let date = `${dateObj.getDate()}-${dateObj.getMonth()}-${dateObj.getFullYear()}`
+
     let card = {
         id: uuidv4(),
         title: recipientTitle.value,
         description: descriptionText.value,
-        date: "may 6",
+        date,
         status: "todo"
     }
 
-    cards.push(card)
-    load();
+    let response = await fetch("https://box-board.herokuapp.com/api/cards", {
+        method: 'POST', 
+        headers: {
+            'Content-Type': 'application/json'
+          },
+        body: JSON.stringify(card)
+    })
+
+    if (response.ok){
+        recipientTitle.value = "";
+        descriptionText.value = ""; 
+        load();
+    } else {
+        // Alert
+    }
+
+    
+
 }
 
 function uuidv4() {
-    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
     );
-  }
+}
 
-  addCardBtn.addEventListener('click', addCard)
+addCardBtn.addEventListener('click', addCard)
